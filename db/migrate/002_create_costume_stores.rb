b001a2d29f0280aa3e95c4c6606167b31d851a12
Nathalie Santana <nathalie.scruz@gmail.com>
2018-10-01 19:48:12 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.1]
+  def change 
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees_count
+      t.boolean :open? 
+      t.timestamps :opening_time
+      t.timestamps :closing_time
+      t.timestamps
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
 # Create your haunted_houses migration here
+
+class HauntedHouses < ActiveRecord::Migration[5.1]
+  def change 
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :friendly?
+      t.timestamps :opening_date
+      t.timestamps :closing_date
+      t.text :description
+    end
+  end
+end
