3ef5f79c30da7768964b64544368e9ba31ac9323
Nathalie Santana <nathalie.scruz@gmail.com>
2018-10-01 19:54:36 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,7 +7,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :location
       t.string :theme
       t.float :price
-      t.boolean :friendly?
+      t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :description
