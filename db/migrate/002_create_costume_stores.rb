afb29613d1090dfe0912d7292645a9757a7c6917
Nathalie Santana <nathalie.scruz@gmail.com>
2018-10-01 19:52:46 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,8 +5,8 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :employees_count
-      t.boolean :open? 
+      t.integer :num_of_employes
+      t.boolean :still_in_business 
       t.datetime :opening_time
       t.datetime :closing_time
       t.timestamps
